ff4acab9820f4f997e7f36c6e2bda826404fde99
ShailjaJhunjhunwala <68336814+ShailjaJhunjhunwala@users.noreply.github.com>
2020-10-28 14:06:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var dog, happyDog,dogHappy, database, foodS, foodStock;
+
+function preload()
+{
+  dogHappy = loadImage("images/dogImg.png")
+  happyDog = loadImage("images/dogImg1.png")
+}
+
+function setup() {
+  createCanvas(500,500);
+  database = firebase.database()
+  console.log(database)
+  dog = createSprite(250,250,20,20)
+  dog.addImage(dogHappy)
+  dog.scale = 0.2
+  foodStock = database.ref("food")
+  foodStock.on("value",readStock)
+}
+
+
+function draw() {  
+  background(46, 139, 87)
+  if(keyDown(UP_ARROW)){
+    foodS--
+    writeStock(foodS);
+    dog.addImage(happyDog)
+}
+  drawSprites();
+}
+function readStock(data){
+foodS = data.val();
+console.log(foodS)
+}
+function writeStock(x){
+database.ref("/").update({
+  food:x
+})
+}
+
+
+
